57d450b42e07a7ed0c866912c6a77a5dbb38b2f0
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2025-06-06 00:21:55 +0200

    feat: add lingo.dev translations
    These are added automatically by the compiler when you run npm build

---
+++ src/lingo/dictionary.js
@@ -0,0 +1,45 @@
+export default {
+  version: 0.1,
+  files: {
+    "app/page.tsx": {
+      entries: {
+        "0/declaration/body/0/argument/1": {
+          content: {
+            de: "Einfache statische App",
+            en: "Simple Static App",
+            es: "Aplicación Estática Simple",
+            fr: "Application statique simple",
+          },
+          hash: "f0a4ada26172730ae6fd1cf84ac4b10a",
+        },
+        "0/declaration/body/0/argument/3": {
+          content: {
+            de: "Dies ist eine einfache statische App, die verwendet werden kann, um die Integration des Ling.dev Compilers zu testen.",
+            en: "This is a simple static app that can be used to test Ling.dev Compiler integration.",
+            es: "Esta es una aplicación estática simple que se puede usar para probar la integración del compilador de Ling.dev.",
+            fr: "C'est une application statique simple qui peut être utilisée pour tester l'intégration du compilateur Ling.dev.",
+          },
+          hash: "11b51a9ba83ceff95b24b270cb2b5d34",
+        },
+        "0/declaration/body/0/argument/5": {
+          content: {
+            de: "Dies ist ein Text, der verwendet werden kann, um die Übersetzung zu testen.",
+            en: "This is some text that can be used to test the translation.",
+            es: "Este es un texto que se puede usar para probar la traducción.",
+            fr: "Ceci est un texte qui peut être utilisé pour tester la traduction.",
+          },
+          hash: "25991716ed68eef79ddb6e80f78c482c",
+        },
+        "0/declaration/body/0/argument/7/1": {
+          content: {
+            de: "Klicken Sie mich",
+            en: "Click me",
+            es: "Haz clic aquí",
+            fr: "Cliquez-moi",
+          },
+          hash: "9e6cf86f33db401a10e0e8494db1854d",
+        },
+      },
+    },
+  },
+};
